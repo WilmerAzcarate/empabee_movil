9f6cf282c7fe015bb2eabd4e10af4ddca17ad727
Jhon Wilmer Azcárate Rivera <unavidahonorable@hotmail.com>
2024-02-25 22:01:15 -0500

    UPDATE - Dashboard implementado

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -5,7 +5,8 @@ const colorList = <Color>[Colors.orange];
 class AppTheme {
   final int selectedColor;
   AppTheme({this.selectedColor = 0})
-      : assert(selectedColor < colorList.length, 'Selected color must be less or equal than ${colorList.length-1}');
+      : assert(selectedColor>=0,'El color seleccionado debe ser mayor o igual que 0'),
+        assert(selectedColor < colorList.length, 'El color seleccionado debe ser menor o igual que ${colorList.length-1}');
 
   ThemeData getTheme() =>
       ThemeData(
--- lib/presentation/screens/dashboard/dashboard_screens.dart
+++ lib/presentation/screens/dashboard/dashboard_screens.dart
@@ -1,3 +1,4 @@
+import 'package:empabee_movil/config/menu/menu_item.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -9,6 +10,45 @@ class DashboardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
+      body: const _DashboardView(),
+    );
+  }
+}
+
+class _DashboardView extends StatelessWidget {
+  const _DashboardView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context,index){
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      }
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+  
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading:Icon(menuItem.icon,color:colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded,color:colors.secondary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: (){
+        print('Hello world');
+      },
     );
   }
 }
